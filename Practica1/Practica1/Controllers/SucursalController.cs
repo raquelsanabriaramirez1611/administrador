6a7c961cf00d5bb737e9ccd24017e300150b47a2
RaquelSanabriaRamirez <rasara1612@gmail.com>
2025-06-05 17:08:51 -0600

    Proyecto Inicial

---
+++ Practica1/Practica1/Controllers/SucursalController.cs
@@ -0,0 +1,117 @@
+﻿using Practica1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Practica1.Controllers
+{
+    public class SucursalController : Controller
+    {
+        // GET: Sucursal
+        public ActionResult Index()
+        {
+            try
+            {
+                using (Practica1Entities db = new Practica1Entities())
+                {
+                    var sucursales = db.Sucursal.Select(s => new Models.ViewModels.cListaSucursales
+                    {
+                        SucursalID = s.SucursalId,
+                        Nombre = s.Nombre,
+                        Direccion = s.Direccion,
+                        Telefono = s.Telefono
+                    }).ToList();
+
+                    return View(sucursales);
+                }
+            }
+            catch (Exception ex)
+            {
+                return View(new List<Models.ViewModels.cListaSucursales>()); // Return an empty list in case of error        
+
+
+            }
+        }
+
+
+
+        // GET: Sucursal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Sucursal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    
+        // POST: Sucursal/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // GET: Sucursal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Sucursal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sucursal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Sucursal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+
+    internal class Practica1Entities1
+    {
+    }
+}
---
+++ Practica1/Practica1/Models/ViewModels/cListaSucursales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica1.Models.ViewModels
+{
+    public class cListaSucursales
+    {
+        public int SucursalID { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
