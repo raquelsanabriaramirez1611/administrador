f1da6194a3653ef9e3bb7a24989f133964da45a0
RaquelSanabriaRamirez <rasara1612@gmail.com>
2025-06-16 20:20:11 -0600

    Crud

--- Practica1/Practica1/Controllers/SucursalController.cs
+++ Practica1/Practica1/Controllers/SucursalController.cs
@@ -3,6 +3,7 @@ using Practica1.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web.Mvc;
 
 namespace Practica1.Controllers
@@ -68,50 +69,131 @@ namespace Practica1.Controllers
             return View();
         }
 
-    
+
         // POST: Sucursal/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(createSucursal sucursal)
         {
             try
             {
-                // TODO: Add insert logic here
+                if (!ModelState.IsValid) 
+                    {
+                        return View(sucursal);
+                    }
+                    using(Practica1Entities db = new Practica1Entities())
+                    {
+                        Sucursal newSucursal = new Sucursal
+                        {
+                            Nombre = sucursal.Nombre,
+                            Direccion = sucursal.Direccion,
+                            Telefono = sucursal.Telefono
+                        };
 
-                return RedirectToAction("Index");
-            }
-            catch
+                        db.Sucursal.Add(newSucursal);
+                        db.SaveChanges();
+                        ViewBag.MensajeProceso = "Sucursal creada exitosamente";
+                        ViewBag.ValorMensaje = 1;
+                        return View(sucursal);
+                    }
+                }
+            catch(Exception ex)
             {
-                return View();
+                ViewBag.MensajeProceso = "Error al crear la sucursal " + ex.Message;
+                ViewBag.ValorMensage = 0;
+                return View(sucursal);
             }
-        }
+
+            }               
 
 
         // GET: Sucursal/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            try
+            {
+                editSucursal sucursal = new editSucursal();
+                using (Practica1Entities db = new Practica1Entities())
+                {
+                    var sucursalData = db.Sucursal.FirstOrDefault(s => s.SucursalId == id);
+                    if (sucursalData != null)
+                    {
+                        sucursal.SucursalID = sucursalData.SucursalId;
+                        sucursal.Nombre = sucursalData.Nombre;
+                        sucursal.Direccion = sucursalData.Direccion;
+                        sucursal.Telefono = sucursalData.Telefono;
+                    }
+                    return View(sucursal);
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("Error", new HandleErrorInfo(ex, "Sucursal", "Edit"));
+            }
         }
 
         // POST: Sucursal/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(editSucursal sucursal)
         {
             try
             {
-                // TODO: Add update logic here
+                if (!ModelState.IsValid)
+                {
+                    return View(sucursal);
+                }
+                using (Practica1Entities db = new Practica1Entities())
+                {
+                    var sucursalActual = db.Sucursal.Find(sucursal.SucursalID);
+                    if (sucursalActual != null)
+                    {
+                        sucursalActual.Nombre = sucursal.Nombre;
+                        sucursalActual.Direccion = sucursal.Direccion;
+                        sucursalActual.Telefono = sucursal.Telefono;
 
-                return RedirectToAction("Index");
+
+
+                        db.Entry(sucursalActual).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        ViewBag.MensajeProceso = "Sucursal editada exitosamente";
+                        ViewBag.ValorMensaje = 1;
+
+                    }
+                    return View(sucursal);
+                }
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.MensajeProceso = "Error al editar la sucursal " + ex.Message;
+                ViewBag.ValorMensage = 0;
+                return View(sucursal);
             }
         }
 
         // GET: Sucursal/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            try
+            {
+                using (Practica1Entities db = new Practica1Entities())
+                {
+                    var sucursal = db.Sucursal.Find(id);
+                    if (sucursal != null)
+                    {
+                        db.Sucursal.Remove(sucursal);
+                        db.SaveChanges();
+                        TempData["MensajeEliminado"] = "Sucursal eliminada exitosamente";
+                        TempData["ValorMensaje"] = 0;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                TempData["MensajeEliminado"] = "Error al eliminar la sucursal " + ex.Message;
+                TempData["ValorMensaje"] = 0;
+            }
+            return RedirectToAction("Index");
+           
         }
 
         // POST: Sucursal/Delete/5
---
+++ Practica1/Practica1/Models/ViewModels/editSucursal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica1.Models.ViewModels
+{
+    public class editSucursal : createSucursal
+    {
+        public int SucursalID { get; set; }
+    }
+}
